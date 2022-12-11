02e3bca4e708294dc3527a45dec3bd51db5f16c8
Kauã <98705115+KauaZs@users.noreply.github.com>
2022-12-11 09:50:15 -0300

    identa

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -13,9 +13,9 @@ module.exports = class extends Event {
 
     if(message.content.replace('!','').startsWith(`<@${this.client.user.id}>`)) return message.reply(`💻 › Olá \`${message.author.tag}\` meu nome é **Nexter**, sou uma \`API\` de utilidades e funcionalidades.`)
     if(!message.content.startsWith(prefix)) return;
-   const args = message.content.slice(prefix.length).trim().split()
+    const args = message.content.slice(prefix.length).trim().split()
     const cmd = args.shift().toLowerCase()
     const command = this.client.commands.get(cmd) || this.client.commands.find(x => x.aliases.includes(cmd));
-   if(command) return command.run(message, args)
+    if(command) return command.run(message, args)
   }
 }
